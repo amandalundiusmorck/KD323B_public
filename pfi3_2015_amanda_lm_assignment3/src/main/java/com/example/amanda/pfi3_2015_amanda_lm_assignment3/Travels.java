ce4897123ca42977c3fa724229e13d0bafe835b1
Amanda <amandalundius@hotmail.com>
2015-04-16 11:36:50 +0200

    Initial commit

---
+++ pfi3_2015_amanda_lm_assignment3/src/main/java/com/example/amanda/pfi3_2015_amanda_lm_assignment3/Travels.java
@@ -0,0 +1,33 @@
+package com.example.amanda.pfi3_2015_amanda_lm_assignment3;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Travels extends Fragment implements View.OnClickListener{
+
+
+    public Travels() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_travels, container, false);
+    }
+
+public void onClick(View v){
+
+    }
+
+}
